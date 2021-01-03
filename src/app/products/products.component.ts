6da941c869252ffb1744bb67f462d1ec97ebd551
Almir Almiev <madarat@mail.ru>
2021-01-03 14:34:21 +0300

    added products.component

---
+++ src/app/products/products.component.ts
@@ -0,0 +1,18 @@
+import {Component, OnInit} from '@angular/core';
+import {HttpService} from '../services/http.service';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent implements OnInit {
+  public products;
+
+  constructor(private http: HttpService) {
+  }
+
+  ngOnInit(): void {
+    this.products = this.http.getProducts();
+  }
+}
